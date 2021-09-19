6d1af7ef36eef7d0ae6f955d09f5680269326ac3
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2021-09-19 10:23:57 +0500

    alice not visible in release mode and launcher app icon changed

--- lib/src/core/di/http_service.dart
+++ lib/src/core/di/http_service.dart
@@ -1,5 +1,6 @@
 import 'package:alice/alice.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:ox_system_task/src/core/constants/app_constants.dart';
 
 class HttpService {
@@ -14,21 +15,22 @@ class HttpService {
   HttpService() {
     dio = Dio(
       BaseOptions(
-        baseUrl:
-            '${Constants.baseUrlPrefix}',
+        baseUrl: '${Constants.baseUrlPrefix}',
         connectTimeout: 5000,
         receiveTimeout: 5000,
         sendTimeout: 5000,
       ),
-    )..interceptors.addAll(
-        [
-          LogInterceptor(
-            request: false,
-            requestHeader: false,
-            responseHeader: false,
-          ),
-          alice.getDioInterceptor(),
-        ],
+    );
+    dio.interceptors.add(
+      LogInterceptor(
+        request: false,
+        requestHeader: false,
+        responseHeader: false,
+      ),
+    );
+    if (kDebugMode)
+      dio.interceptors.add(
+        alice.getDioInterceptor(),
       );
   }
 }
